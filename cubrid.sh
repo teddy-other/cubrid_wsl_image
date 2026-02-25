590b348fb0d76f0bc96aa530389c7f9fa7797cb6
dev1 <dev1@cubrid.com>
2026-02-25 13:07:35 +0900

    last image

--- cubrid.sh
+++ cubrid.sh
@@ -5,7 +5,7 @@ LD_LIBRARY_PATH=$CUBRID/lib:$CUBRID/cci/lib:$LD_LIBRARY_PATH
 PATH=$CUBRID/bin:/usr/sbin:$PATH
 export LD_LIBRARY_PATH SHLIB_PATH LIBPATH PATH
 
-if [ -f /home/cubrid/entrypoint.sh ]; then
-     /home/cubrid/entrypoint.sh
-     rm /home/cubrid/entrypoint.sh
-fi
+# if [ -f /home/cubrid/entrypoint.sh ]; then
+#      /home/cubrid/entrypoint.sh
+#      rm /home/cubrid/entrypoint.sh
+# fi
